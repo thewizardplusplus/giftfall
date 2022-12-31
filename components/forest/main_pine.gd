6b33a488c128ef7f2c687b0501187f04ba4da1ad
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-01-01 01:47:32 +0300

    Place the rest of pines

--- components/forest/main_pine.gd
+++
@@ -1,6 +0,0 @@
-extends Spatial
-
-onready var _terrain: Node = get_tree().get_root().get_node("/root/root/terrain")
-
-func _ready():
-	self.transform.origin.y = _terrain.get_interpolated_height_at(self.transform.origin)
